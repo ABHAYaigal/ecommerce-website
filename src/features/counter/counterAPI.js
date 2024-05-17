49565aa735cfed624b125511704400a7ba583d51
ABHAYaigal <abhayaigal2398@gmail.com>
2024-05-17 18:01:39 +0530

    this is ecommerce

--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,6 +1,13 @@
-// A mock function to mimic making an async request for data
 export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
-  );
+  return new Promise(async (resolve) => { // Mark the callback function as async
+    try {
+      const response = await fetch('http://localhost:8080');
+      const data = await response.json();
+      resolve({ data });
+    } catch (error) {
+      // Handle any errors here
+      console.error('Error fetching data:', error);
+      resolve({ error: 'Error fetching data' });
+    }
+  });
 }
